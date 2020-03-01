12bb338778c06a4d1eb8f2d65b28f9ce2f190a19
sankhadip samanta <sankhsamanta.cse17@chitkarauniversity.edu.in>
2020-03-01 09:39:52 +0530

    feat: usermodel created, login/signup added

--- server/user/models.py
+++ server/user/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class UserModel(models.Model):
+    userid = models.TextField(max_length = 20)
+    username = models.TextField(max_length = 20)
+    email = models.TextField(max_length = 20)
+    password = models.TextField(max_length = 100)
+    phone = models.TextField(max_length = 12)
+    permission = models.TextField(max_length = 10)
+    usertype = models.TextField(max_length=10)
--- server/user/views.py
+++ server/user/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def login(request):
+    pass
+
+def signup(request):
+    pass
