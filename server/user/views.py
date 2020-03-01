ba6601864fcd2dede1a12c816184ca46c9bb265c
sankhadip samanta <sankhsamanta.cse17@chitkarauniversity.edu.in>
2020-03-01 12:13:42 +0530

    feat: signup implemented

--- server/user/views.py
+++ server/user/views.py
@@ -1,8 +1,48 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse, HttpResponseNotAllowed, HttpResponseBadRequest, HttpResponseServerError
+from django.db import connection
+import json
+import string
+import random
 
 
+cursor=connection.cursor()
+
+
+@csrf_exempt
 def login(request):
-    pass
+    if request.method=='POST':
+        values=json.loads(request.body.decode('utf-8'))
+        print(values)
+        return JsonResponse({ 'code':200 })
+    else:
+        return HttpResponseNotAllowed('Not allowed')
 
+
+
+
+@csrf_exempt
 def signup(request):
-    pass
+    if request.method=='POST':
+        value=json.loads(request.body.decode('utf-8'))
+        userid, username, email, password, cpassword, phone, permission=value.values()
+        try:
+            cursor.execute(f"select * from user_usermodel where username='{username}' or email='{email}'")
+            isUser=cursor.fetchall()
+            if len(isUser)==0:
+                cursor.execute(f"insert into user_usermodel values('{random.getrandbits(128)}', '{userid}', '{username}', '{email}', '{password}', '{phone}','{permission}', 'user')")
+                return JsonResponse({ "code":201 })
+            else:
+                return HttpResponseBadRequest("User already exists")
+        except Exception as e:
+            print(e)
+    else:
+        return HttpResponseNotAllowed('Not allowed')
+
+
+def generateRandomString():
+    N = 7
+    res = ''.join(random.choices(string.ascii_uppercase + string.digits, k = N))
+    print(res)
+    return res
