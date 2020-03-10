1f0b629a4a86b562f0b6ba9a5d9366b91e351346
Sankhadip Samanta <sankhadip.2000@gmail.com>
2020-03-10 10:14:37 +0530

    feat: models created, indention, space fixed

--- server/task/models.py
+++ server/task/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class TaskModel(models.Model):
+    taskid = models.TextField(max_length=5)
+    taskdes = models.TextField(max_length=100)
+    tasktype = models.TextField(max_length=10)
+    owner = models.TextField(max_length=15)
+    status = models.TextField(max_length=6)
